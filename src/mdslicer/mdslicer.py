20d2731062dd31f2fdbae4948bf025ef3427d1d8
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2024-10-29 14:43:58 +0100

    Implement mdslicer

--- src/mdslicer/mdslicer.py
+++ src/mdslicer/mdslicer.py
@@ -1,20 +1,79 @@
-"""Main module."""
+"""Parse markdown content"""
 
-import argparse
-import sys
-
-
-def main():
-    """Console script for mdslicer."""
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('_', nargs='*')
-    args = parser.parse_args()
-
-    print("Arguments: " + str(args._))
-    print("Replace this message by putting your code into "
-          "mdslicer.mdslicer.main")
-    return
+import markdown
+from markdown.extensions import toc
+import bs4
 
 
-if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+md = markdown.Markdown(extensions=["toc"])
+
+
+def md_slugify(value: str) -> str:
+    """
+    slugify string using markdown extension slugifier
+
+    Args:
+        value: string to slugify
+
+    Returns:
+        str: slugified string
+    """
+    return toc.slugify(value, "-")
+
+
+def get_sections(md_content: str) -> tuple[list[dict], list[dict]]:
+    """
+    Parse markdown string to build a list of section dicts:
+    
+    >>> sections, toc = get_sections(md_content)
+    >>> print(section[0])
+    {
+        'title': h2 title,
+        'id': slugify(title),
+        'content': section_content
+    }
+    
+
+    Also return the table of content tokens
+
+    Args:
+        md_content: markdown content
+
+    Returns:
+        tuple: list of section dicts, list of toc tokens
+    """
+
+    # Important for performance:
+    # see https://python-markdown.github.io/extensions/api/#registerextension
+    md.reset()
+    html = md.convert(md_content)
+    # Build section dict
+    soup = bs4.BeautifulSoup(html, "html.parser")
+    section_dict = {}
+
+    # If section does not start with a h2 tag
+    no_h2_section = ""
+    for tag in soup:
+        if isinstance(tag, bs4.Tag) and tag.name == "h2":
+            break
+        else:
+            no_h2_section += str(tag)
+
+    if no_h2_section:
+        section_dict[""] = no_h2_section
+
+    # Parse the rest
+    for h2 in soup.find_all("h2"):
+        title = h2.text
+        section_dict[title] = ""
+        for tag in h2.next_siblings:
+            if isinstance(tag, bs4.Tag) and tag.name == "h2":
+                break
+            section_dict[title] += str(tag)
+
+    sections = []
+    for title, content in section_dict.items():
+        section = {"title": title, "id": md_slugify(title), "content": content}
+        sections.append(section)
+
+    return sections, md.toc_tokens
--- tests/test_mdslicer.py
+++ tests/test_mdslicer.py
@@ -1,16 +1,90 @@
-import pytest
+from faker import Faker
+
 from mdslicer import mdslicer
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    pass
+fake = Faker()
+Faker.seed(4321)
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    pass
+address = fake.address()
+
+formatted_address = "".join(f"> {line}\n" for line in address.split("\n"))
+
+md_sections = f"""\
+{fake.text()}
+
+It's address is:
+
+{formatted_address}
+
+## {fake.sentence(nb_words=6)[:-1]}
+
+You may want to see [StrasMap](https://strasmap.eu/Home).
+
+## {fake.sentence(nb_words=5)[:-1]}
+
+### {fake.sentence(nb_words=3)[:-1]}
+
+- {fake.sentence(nb_words=6)[:-1]},
+- {fake.sentence(nb_words=2)[:-1]},
+- {fake.sentence(nb_words=6)}
+"""
+
+
+expected_parsed_sections = [
+    {
+        "title": "",
+        "id": "",
+        "content": """\
+<p>See possible world personal decision. Itself detail out create doctor social. Hope attention friend peace create each.
+House laugh health price its. Federal lot next senior.</p>
+<p>It's address is:</p>
+<blockquote>
+<p>25102 Tom Loop Apt. 667
+South Adam, ND 20695</p>
+</blockquote>
+""",
+    },
+    {
+        "title": "Help today forget tell positive could yeah",
+        "id": "help-today-forget-tell-positive-could-yeah",
+        "content": """
+<p>You may want to see <a href="https://strasmap.eu/Home">StrasMap</a>.</p>
+""",
+    },
+    {
+        "title": "Democrat ago stock if end place",
+        "id": "democrat-ago-stock-if-end-place",
+        "content": """
+<h3 id="set-design">Set design</h3>
+<ul>
+<li>For book material I accept,</li>
+<li>Cause,</li>
+<li>Pattern both ball bit base fall rule.</li>
+</ul>""",
+    },
+]
+
+
+def test_get_sections():
+    sections, toc = mdslicer.get_sections(md_sections)
+    assert sections == expected_parsed_sections
+
+    print(toc)
+    toc_1 = toc[0]
+    assert toc_1["children"] == []
+    assert toc_1["id"] == "help-today-forget-tell-positive-could-yeah"
+    assert toc_1["level"] == 2
+    assert toc_1["name"] == "Help today forget tell positive could yeah"
+
+    toc_2 = toc[1]
+    toc2_child = toc_2["children"][0]
+    assert toc2_child["children"] == []
+    assert toc2_child["id"] == "set-design"
+    assert toc2_child["level"] == 3
+    assert toc2_child["name"] == "Set design"
+
+    assert toc_2["id"] == "democrat-ago-stock-if-end-place"
+    assert toc_2["level"] == 2
+    assert toc_2["name"] == "Democrat ago stock if end place"
